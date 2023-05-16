672ac318ca097d3f83a69d244c45579d24f3247d
LordOfShadow666 <51244051+LordOfShadow666@users.noreply.github.com>
2023-05-16 19:29:07 +0400

    первые пробы

---
+++ MedicalSimulatorTest/Assets/MedicalSimulatorTest/Scripts/EnterIn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterIn : MonoBehaviour
+{
+    //public GameObject examCode;
+    // Start is called before the first frame update
+    void Start()
+    {
+         Debug.Log("GOOD");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
